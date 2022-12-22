5cac4cddf3f973b1160d4922bf305fc51d39b646
Torward <76395706+Torward@users.noreply.github.com>
2022-12-22 14:32:39 +1000

    Front Service

---
+++ static/order/order.js
@@ -0,0 +1,13 @@
+angular.module('market-front').controller('orderController', function ($scope, $location, $http, $localStorage) {
+    const contextPath = 'http://localhost:5555/core/api/v1';
+
+    $scope.loadOrders = function () {
+        $http.get(contextPath + '/orders')
+            .then(function (response) {
+                $scope.MyOrders = response.data;
+            });
+    };
+
+    $scope.loadOrders();
+
+});
